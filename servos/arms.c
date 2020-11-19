a7a8d94aa94037d21154762c29d3cd22656b7219
Bob <bob@herzberg.net>
2020-11-18 16:53:56 -0800

    add string.h

--- servos/arms.c
+++ servos/arms.c
@@ -1,32 +1,3 @@
-/*************************************************************************
- * servo.c
- *
- * PCA9685 servo example
- * Connect a servo to any pin. It will rotate to random angles.
- *
- *
- * This software is a devLib extension to wiringPi <http://wiringpi.com/>
- * and enables it to control the Adafruit PCA9685 16-Channel 12-bit
- * PWM/Servo Driver <http://www.adafruit.com/products/815> via I2C interface.
- *
- * Copyright (c) 2014 Reinhard Sprung
- *
- * If you have questions or improvements email me at
- * reinhard.sprung[at]gmail.com
- *
- * This software is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The given code is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You can view the contents of the licence at <http://www.gnu.org/licenses/>.
- **************************************************************************
- */
 
 #include "pca9685.h"
 
@@ -34,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define PIN_BASE 300
 #define MAX_PWM 4096
