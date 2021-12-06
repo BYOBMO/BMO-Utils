8d23a6496cdeb64e5e31ac0b3ddf25d314a70e6f
orbian <bob@herzberg.net>
2021-12-06 11:56:26 -0800

    Update google-voice.sh

--- voice/google-voice.sh
+++ voice/google-voice.sh
@@ -3,5 +3,5 @@
 #export LD_LIBRARY_PATH="/home/pi/bmos/gv/opt"
 #export GOOGLE_APPLICATION_CREDENTIALS="/home/pi/bmos/credentials.json"
 
-python /home/pi/bmos/gv/transcribe.py
+python3 /home/pi/bmos/gv/transcribe.py
 rm out.wav
