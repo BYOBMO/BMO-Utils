e4d9db6dfb83cde83f6d9948160958bc023ed6e0
orbian <bob@herzberg.net>
2023-07-14 16:24:16 -0700

    Debig voice

--- voice/transcribe.py
+++ voice/transcribe.py
@@ -1,9 +1,19 @@
 import speech_recognition as sr
 r = sr.Recognizer()
-af = sr.AudioFile('out.wav')
-with af as source:
-   audio = r.record(source)
+try:
+   af = sr.AudioFile('out.wav')
+   with af as source:
+      audio = r.record(source)
 
-x = r.recognize_google(audio)
-print('"' + x + '"')
+   try:
+      x = r.recognize_google(audio)
+      print('"' + x + '"')
+   except sr.UnknownValueError:
+       print("ERR: Google Speech Recognition could not understand audio")
+   except sr.RequestError as e:
+       print("ERR: Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
+except sr.RequestError as e:
+   print("ERR: audio file; {0}".format(e))
 
